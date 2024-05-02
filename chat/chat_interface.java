1b77621b74965cd5263f721f87029627feb11c71
legiangminhnhien <nhiensu0905@gmail.com>
2024-05-02 10:00:24 +0700

    interface chat

---
+++ chat/chat_interface.java
@@ -0,0 +1,12 @@
+package group1.chat;
+
+import javax.swing.*;
+
+public class chat_interface {
+    private JPanel panel1;
+    private JLabel welcomeToJiraChatLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
